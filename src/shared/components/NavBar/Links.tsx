21bca7a28d7d7267c1a695159830322688bf8974
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-04 15:36:50 -0600

    chore: add navbar fix logic

--- src/shared/components/NavBar/Links.tsx
+++ src/shared/components/NavBar/Links.tsx
@@ -8,14 +8,11 @@ function Links() {
         Object.entries(routesContstants).map(([_, value], i) => {
           const route = value[0];
           return (
-            <li key={route[1]}>
-              <Link href={route[0]} aria-label={`Go to ${route[1]} page`}>
-                <span
-                >
-                  {route[1]}
-                </span>
-              </Link>
-            </li>
+
+            <Link key={route[1]} href={route[0]} aria-label={`Go to ${route[1]} page`}>
+                {route[1]}
+            </Link>
+
           );
         })
       }
--- src/shared/components/NavBar/index.tsx
+++ src/shared/components/NavBar/index.tsx
@@ -1,31 +1,54 @@
-import Links from "./Links"
-import styles from './NavBar.module.scss'
+"use client"
+import { Luxurious_Script } from 'next/font/google'
+const luxurious = Luxurious_Script({ weight: '400', subsets: ['latin', 'latin-ext', 'vietnamese'] })
+
+import Links from "./Links";
+import { useState } from "react";
+import './scss/index.scss';
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
-    // <nav className={styles.nav} id="nav">
-    //   <div className="nav__container">
-    //     <p className="nav__logo">Leones</p>
-    //     <div className="nav__img" id="menu">
-    //       <div className="linea-1"></div>
-    //       <div className="linea-2"></div>
-    //       <div className="linea-3"></div>
-    //     </div>
+    <>
+      <nav className="nav" id="nav">
+        <div className="nav__container">
+          <p className={`nav__logo ${luxurious.className}`}>Leones</p>
 
-    //     <div className="nav__menu" id="nav_menu">
+          <div className={`nav__img ${isMenuOpen ? 'change' : ''}`} onClick={toggleMenu}>
+            <div className="linea-1"></div>
+            <div className="linea-2"></div>
+            <div className="linea-3"></div>
+          </div>
 
-    //       <div className="cbdark">
-    //         <button className="bdark " id="bdark" />
+          <div className={`nav__menu ${isMenuOpen ? 'despliege' : ''}`}>
+            {
+              // Temporalmente desactivado
+              /* <div className="cbdark">
+                <button className="bdark" id="bdark" />
+              </div> */
+            }
+            <Links />
+          </div>
+        </div>
 
-    //       </div>
 
-    //     <Links />
-    //     </div>
-    //   </div>
-    // </nav>
-    <></>
-  )
+      </nav>
+      <div
+        className={`modal_menu ${isMenuOpen ? "display-block" : ""}`}
+        onClick={closeMenu}
+      >
+      </div>
+    </>
+  );
 }
 
-export default NavBar
+export default NavBar;
