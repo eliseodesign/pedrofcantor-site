042e71a1754d6fe75015a813023e9bfc3b93aed6
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-07 10:35:07 -0600

    chore (admin): :sparkles: add other methods

--- src/database/queries/admin.ts
+++ src/database/queries/admin.ts
@@ -9,12 +9,50 @@ export class AdminQueries implements Querie {
     this.dbHelper = new DatabaseHelper(db);
   }
 
+  async selectOne(id: number){
+    const sql = 'SELECT * FROM Admin WHERE id = ?';
+    const params = [id];
+
+    try {
+      const row = await this.dbHelper.executeQuery(sql, params);
+      return row;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  async selectAll() {
+    const sql = 'SELECT * FROM Admin';
+  
+    try {
+      const rows = await this.dbHelper.executeQueryAll(sql, []);
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
   async insert(user: Admin) {
     const sql = 'INSERT INTO Admin (username, password) VALUES (?,?)';
     const params = [user.username, user.password];
 
-    return this.dbHelper.executeQuery(sql, params)
-      .then(() => ({ success: "admin create" }));
+    try {
+      await this.dbHelper.executeQuery(sql, params);
+      return { success: "admin create" };
+    } catch (error) {
+      throw error;
+    }
   }
-  // todo: delete
+  
+  async delete(id:number) {
+    const sql = 'DELETE FROM Admin WHERE id = ?'
+    const params = [id]
+
+    try {
+      await this.dbHelper.executeQuery(sql, params);
+      return { success: "admin deleted" };
+    } catch (error) {
+      throw error;
+    }
+  }
 }
