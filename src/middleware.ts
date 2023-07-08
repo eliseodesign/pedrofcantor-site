ef299843eb1a4692cb2bad3534e708a545863060
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-08 05:09:24 -0600

    refact (middleware): :hammer: change way to define which paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,12 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export const middleware = (request: NextRequest) => {
-  return NextResponse.redirect(new URL('/inicio', request.url))
-}
-export const config = {
-  matcher: '/',
+  if(request.nextUrl.pathname === "/"){
+    return NextResponse.redirect(new URL('/inicio', request.url))
+  }
+  if(request.nextUrl.pathname === "/super-admin"){
+    // TODO: validar que es super-administrador
+    return NextResponse.redirect(new URL('/inicio', request.url))
+  }
+  return;
 }
