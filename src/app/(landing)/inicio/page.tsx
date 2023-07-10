f1354c9507414805cfe87c8acff70694b5c078a1
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-10 09:38:09 -0600

    fix(database): :fire: delete folder

--- src/app/(landing)/inicio/page.tsx
+++ src/app/(landing)/inicio/page.tsx
@@ -1,22 +1,13 @@
 import { getSEO } from '@/shared/seoData'
 import { Header, Cita} from './components'
-import { AdminQueries } from '@/database/queries/admin'
 
 export const metadata = getSEO("INICIO")
 
 export default async function Home() {
-  const s = new AdminQueries()
-  const result = await s.selectAll()
   return (
     <main>
       <Header />
       <Cita />
-
-      {
-        JSON.stringify(
-          result
-        )
-      }
     </main>
   )
 }
--- src/app/(private)/super-admin/page.tsx
+++ src/app/(private)/super-admin/page.tsx
@@ -1,20 +1,7 @@
-import { AdminQueries } from '@/database/queries/admin'
-
 async function SuperAdmin() {
-  const db = new AdminQueries()
-  const admins = await db.selectAll()
   return (
     <div>
-      {
-        admins.map( admin => {
-          return (
-            <div key={admin.username}>
-              <h1>{admin.username}</h1>
-              <p>{admin.enable ? "cuenta activa":"cuenta desactiva"}</p>
-            </div>
-          )
-        })
-      }
+    
     </div>
   )
 }
