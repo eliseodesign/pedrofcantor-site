555e66dd7c5592dbdf715a33f102bdf7f21d1aae
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-11 16:14:26 -0600

    fix(Articulo): :hammer: change metadata

--- src/shared/interfaces/Articulo.ts
+++ src/shared/interfaces/Articulo.ts
@@ -1,10 +1,7 @@
 export interface Articulo {
   shortname: string, // será la url y el nombre del archivo markdown
-  metadata: Metadata
-} 
-
-export interface Metadata{
   title: string
   description: string
   date: Date
-}
+} 
+
