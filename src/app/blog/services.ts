97a2a22ecdad0c7fa96888f1c4d42b1ad21136be
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-21 11:57:46 -0600

    fix: :hammer: update post dir

--- src/app/blog/services.ts
+++ src/app/blog/services.ts
@@ -4,12 +4,14 @@ import { formatDate } from '@/shared/handlers/formatDate'
 import matter from 'gray-matter';
 import path from 'path'
 
-const blogDir = path.join(process.cwd(), 'src', 'app', 'blog', '(articles)')
+
+const ARTICLES_PATH = 'src/app/blog/(articles)';
+const POSTS_DIR = path.join(process.cwd(), ARTICLES_PATH);
 
 export const getArticles = (): Articulo[] => { 
-  const articles: Articulo[] = fs.readdirSync(blogDir).map(file => {
+  const articles: Articulo[] = fs.readdirSync(POSTS_DIR).map(file => {
 
-    const filePath = path.join(blogDir, file);
+    const filePath = path.join(POSTS_DIR, file);
     const source = fs.readFileSync(filePath, 'utf-8');
     const matterResult = matter(source);
     const { title, type , description, date} = matterResult.data;
