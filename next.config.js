3a2400ecfc9f65f5bee9d616818c828243139537
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-27 20:36:37 -0600

    fix: :hammer: uninstall dependencies

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
-const withMarkdoc = require('@markdoc/next.js');
-
 const path = require('path')
 
-const markdocConfig = {
-  mode: 'static',
-  schemaPath:'./src/app/blog'
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx']
 }
-module.exports = withMarkdoc(markdocConfig)(
-  {
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-    },
-    pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx']
-  }
-);
+
 
