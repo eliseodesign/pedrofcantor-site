fef545169436d744a9724438f49c9c9bc48c5472
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-27 20:36:16 -0600

    fix: :fire: delete code of markdoc

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,91 +1,9 @@
-import { glob } from 'glob';
-import path from 'path';
-import fs from 'fs';
-import { prisma } from '@/lib/prisma'
-import Markdoc from '@markdoc/markdoc';
-import React from 'react';
-import TableOfContents from '../(blog.components)/table.of.contents';
-import matter from 'gray-matter';
-import { Metadata } from 'next';
-import { components, config } from '../config.markdoc';
-import Image from 'next/image'
-import '../markdown.css'
+import React from 'react'
 
-const ARTICLES_PATH = 'src/app/blog/(articles)';
-const POSTS_DIR = path.join(process.cwd(), ARTICLES_PATH);
-
-type Params = {
-    slug: string
+const Blog = () => {
+  return (
+    <div>Blog slug</div>
+  )
 }
 
-type PageProps = {
-    params: Params
-}
-
-export const dynamicParams = false;
-
-export async function generateStaticParams() {
-    const postPaths = await glob(path.join(POSTS_DIR, '**/*.md'));
-    return postPaths.map(postPath => {
-        return { slug: path.basename(postPath, path.extname(postPath)) }
-    });
-}
-
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
-    const { title } = await getMarkdownContent(params.slug);
-    return { title: title }
-}
-
-async function getMarkdownContent(slug: string | undefined) {
-    const filePath = path.join(POSTS_DIR, slug + '.md');
-    const source = fs.readFileSync(filePath, 'utf-8');
-    const matterResult = matter(source);
-    const { title, type } = matterResult.data;
-    const ast = Markdoc.parse(source);
-    const content = Markdoc.transform(ast, config);
-    return { content, title };
-}
-
-function extractHeadings(node: any, sections: any[] = []) {
-    if (node) {
-        if (node.name === 'Heading') {
-            const title = node.children[0];
-
-            if (typeof title === 'string') {
-                sections.push({
-                    ...node.attributes,
-                    title
-                });
-            }
-        }
-
-        if (node.children) {
-            for (const child of node.children) {
-                extractHeadings(child, sections);
-            }
-        }
-    }
-
-    return sections;
-}
-
-export default async function BlogPost({ params }: PageProps) {
-    const { content } = await getMarkdownContent(params.slug);
-    const tableOfContents = extractHeadings(content);
-
-    const articulo = await prisma.articulo.findUnique({ 
-        where: {
-            shortname: params.slug
-        }
-    })
-
-    return (
-        <div className='markdown-container'>
-            {
-                articulo && <Image width={500} height={500} src={`/${articulo!.urlImage}`} alt={`image ${articulo?.title}`}/>
-            }
-            <TableOfContents tableOfContents={tableOfContents} />
-            {Markdoc.renderers.react(content, React, { components })}
-        </div>
-    )
-}
+export default Blog
