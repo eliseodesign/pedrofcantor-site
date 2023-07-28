1a2eac41ec2c3991158c2760f92bf02fb07aaed1
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-28 06:24:06 -0600

    feat(blog): :sparkles: reder articulo

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import MarkdownIt from 'markdown-it';
+import { prisma } from '@/lib/prisma';
+import '../markdown.css'
 
-const Blog = () => {
+const Blog = async ({ params }: { params: { slug: string } }) => {
+  const articulo = await prisma.articulo.findUnique({ where: { shortname: params.slug } });  //select * from
+
+  let html: string | TrustedHTML | undefined;
+  if (articulo) {
+    const md = new MarkdownIt();
+    html = md.render(articulo.content); // convertir md a html
+  }
+
   return (
-    <div>Blog slug</div>
-  )
-}
+    <div className='markdown-container'>
+      {html !== undefined ? <main dangerouslySetInnerHTML={{ __html: html }} /> : null}
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
